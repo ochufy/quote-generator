e5334fda37d1fe8fe7e5146405fb1a09d56fc20d
ochufy <roshancm01@gmail.com>
2022-08-18 16:34:46 +0530

    get new quote on selected tag

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,25 @@
 import Axios from "axios"
 import { useState, useEffect } from "react"
 import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import Tags from "./tags.json";
+import Button from '@mui/material/Button';
+
 
 function Home() {
   const [quote, setQuote] = useState("");
   const [author, setAuthor] = useState("");
 
+  const [tag, setTag] = useState("");
+
   const getQuote = () => {
-    Axios.get("https://api.quotable.io/random")
+    Axios.get(`https://api.quotable.io/random`)
     .then(
       (response) => {
-        console.log(response.data.content);
-        console.log("-",response.data.author);
+        console.log(response.data);
         setQuote(response.data.content);
         setAuthor("- " + response.data.author);
       }
@@ -25,7 +29,22 @@ function Home() {
   useEffect(() => {getQuote()}, [])
 
   const options = Tags.map(tag => (tag.name));
-  
+
+  const getNewQuote = () => {
+    // window.location.reload(false);
+    if(tag != null){
+    Axios.get(`https://api.quotable.io/random?tags=${tag}`)
+    .then(
+      (response) => {
+        console.log("tag = " + response.data.tags);
+        setQuote(response.data.content);
+        setAuthor("- " + response.data.author);
+      }
+    )
+  }
+  else {getQuote()}
+  }
+
   return (
     <>
     <Paper
@@ -47,12 +66,26 @@ function Home() {
       <Typography variant="body2">{author}</Typography>
 
     </Paper>
+    <Box
+    sx={{
+      margin: "auto",
+      display: "flex",
+      justifyContent: 'space-between',
+      flexWrap: 'wrap',
+      alignItems: 'center',
+      flexDirection: "column",
+    }}>
     <Autocomplete
       disablePortal
       options={options}
       sx={{ width: 200, margin: "auto", mt: "3%" }}
+      onChange={(event, newTag) => {setTag(newTag); console.log(newTag,tag);}}
       renderInput={(params) => <TextField {...params} label="choose tag" />}
     />
+    <Button sx={{mt: "3%"}} variant="contained" color="success" onClick={getNewQuote}>
+      Next Quote
+    </Button>
+    </Box>
     </>
   )
 }
