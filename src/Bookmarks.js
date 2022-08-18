30d15b2c7143e77474225cfa60429c2f7bdccb71
ochufy <roshancm01@gmail.com>
2022-08-18 20:22:14 +0530

    show bookmarked quotes in bookmarks page

--- src/Bookmarks.js
+++ src/Bookmarks.js
@@ -1,10 +1,37 @@
 import idlist from "./QuoteIDs.js";
+import { useState, useEffect } from "react"
+import Axios from "axios"
+import QuoteBox from "./QuoteBox.js";
 
 function Bookmarks() {
 
-  console.log(idlist);
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+
+  const [quoteBoxes, setQuoteBoxes] = useState([]);
+
+  function newQuoteBox(id) {
+    Axios.get(`https://api.quotable.io/quotes/${id}`)
+    .then(
+      (response) => {
+        console.log(response.data.content);
+        console.log(response.data.author);
+        setQuote(response.data.content);
+        setAuthor("- " + response.data.author);
+        setQuoteBoxes(quoteBoxes.concat(<QuoteBox quote={quote} author={author}/>))
+      }
+    )
+  }
+
+  if(idlist != null){
+  idlist.forEach(newQuoteBox)}
+
+  useEffect(() => {newQuoteBox()}, [])
+
   return (
-    <></>
+    <>
+      {quoteBoxes}
+    </>
   )
 }
 
---
+++ src/QuoteBox.js
@@ -0,0 +1,29 @@
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+
+function QuoteBox(props) {
+  return(
+    <>
+    <Paper
+      elevation={4}
+      sx={{
+        margin: "auto",
+        mt: "3%",
+        display: "flex",
+        justifyContent: 'center',
+        flexWrap: 'wrap',
+        alignItems: 'center',
+        backgroundColor: '#D05252',
+        borderRadius: "20px",
+        width: "50%",
+        padding: "2% 2% 1% 2%"
+      }}
+    >
+      <Typography variant="body1" sx={{color: "white", width:"100%",pb:"7%", display:"flex", alignItems:"center",justifyContent: 'center',}}>{props.quote}</Typography>
+      <Typography sx={{ml: "10%",color: "white", fontWeight: "600"}} variant="body2">{props.author}</Typography>
+    </Paper>
+    </>
+  )
+}
+
+export default QuoteBox;
