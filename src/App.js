97161b2bebf488ad4a31ab009f2b97f25701443c
ochufy <roshancm01@gmail.com>
2022-08-18 14:40:05 +0530

    change font to Poppins

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import {Route, Routes} from "react-router-dom";
 import Home from "./Home.js";
 import Bookmarks from "./Bookmarks.js";
+import MenuButtons from "./MenuButtons.js";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 function App() {
+  const theme1 = createTheme(
+    {
+      typography: {
+        fontFamily: "Poppins",
+        fontSize: 16,
+      }
+    }
+  );
+
+
+
   return (
+    <ThemeProvider theme={theme1}>
+    <MenuButtons />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="bookmarks" element={<Bookmarks />} />
     </Routes>
+    </ThemeProvider>
   );
 }
 
