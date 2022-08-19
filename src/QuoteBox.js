e2c3efc2a15587d87db6859ec293ea626f28c3dc
ochufy <roshancm01@gmail.com>
2022-08-19 10:42:30 +0530

    add delete icon

--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -1,9 +1,20 @@
 import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
-import { useEffect } from "react";
+import IconButton from '@mui/material/IconButton';
+import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
+import {favQuotes} from "./FavQuotes.js";
+
 function QuoteBox(props) {
 
-  useEffect(()=>{},[])
+  const removeQuote = () => {
+    console.log(props);
+    console.log(favQuotes);
+    favQuotes = favQuotes.filter(quote => quote.id !== props.id);
+    console.log(favQuotes);
+  }
+
   return(
     <>
     <Paper
@@ -21,8 +32,18 @@ function QuoteBox(props) {
         padding: "2% 2% 1% 2%"
       }}
     >
+
       <Typography variant="body1" sx={{color: "white", width:"100%",pb:"7%", display:"flex", alignItems:"center",justifyContent: 'center',}}>{props.quote}</Typography>
-      <Typography sx={{ml: "3%",color: "white", fontWeight: "600"}} variant="body2">{props.author}</Typography>
+      <Box sx={{width: "100%"}}>
+        <Grid container spacing={2}>
+          <Grid item xs={10} sx={{display:"flex", justifyContent:"center",alignItems:"center"}}>
+            <Typography sx={{ml: "15%",color: "white", fontWeight: "600"}} variant="body2">{props.author}</Typography>
+          </Grid>
+          <Grid item xs={2} sx={{display:"flex", justifyContent:"center",alignItems:"center"}}>
+            <IconButton onClick={removeQuote}><RemoveCircleOutlineIcon sx={{color: "white"}} fontSize="small"/></IconButton>
+          </Grid>
+        </Grid>
+      </Box>
     </Paper>
     </>
   )
